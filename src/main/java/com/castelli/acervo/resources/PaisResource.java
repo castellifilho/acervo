15ea7f789bb9b3e79226ea035245903be8c628cb
Castelli <isisdare@gmail.com>
2020-03-27 21:55:06 -0300

    Criando a classe Pais e banco de dados H2

--- src/main/java/com/castelli/acervo/resources/PaisResource.java
+++ src/main/java/com/castelli/acervo/resources/PaisResource.java
@@ -1,15 +1,26 @@
 package com.castelli.acervo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.castelli.acervo.domain.Pais;
 
 @RestController
 @RequestMapping(value="/paises")
 public class PaisResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Pais> find() {
+		Pais p1 = new Pais(1, "Alemanha");
+		Pais p2 = new Pais(2, "Brasil");
+		List<Pais> lista = new ArrayList<>();
+		lista.add(p1);
+		lista.add(p2);
+		
+		return lista;
 	}
 }
